8e9a10ca53e1cac6646cb88ed69b20714fa457f7
FightingHao <496915484@qq.com>
2020-03-08 01:11:28 +0800

    feat: 新增数据模型

---
+++ client/src/pages/DiscussPage/MarkType.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="mark">{{markData}}</div>
+</template>
+
+<script>
+export default {
+  props: ['markData'],
+  data () {
+    return {}
+  },
+  mounted () {}
+}
+</script>
+
+<style lang="scss" scoped>
+.mark {
+  border-radius: 11px;
+  border: 1px solid #ddd;
+  display: inline-block;
+  padding: 3px 11px;
+  color: #333;
+  font-size: 12px;
+  margin-right: 8px;
+  background: #fff;
+  margin-bottom: 5px;
+  line-height: 16px;
+
+  &:hover {
+    border-color: #1abc9c;
+    color: #25bb9b;
+  }
+}
+</style>
