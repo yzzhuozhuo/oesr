6ed515eb4d6a2e764d0f8ffed5f045d1e8bf860e
yangzhuo <1585171898@qq.com>
2020-03-08 18:49:47 +0800

    增加日历模板

--- client/src/pages/WriteCalendar/index.vue
+++ client/src/pages/WriteCalendar/index.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="main">
-    <h3>这是笔试日历页面,haohao是猪^(*￣(oo)￣)^</h3>
+    <div class="header-title">求职 > 校招日历</div>
+    <div class="calendar">
+      <el-calendar class="calendar-content">
+        <!-- 这里使用的是 2.5 slot 语法，对于新项目请使用 2.6 slot 语法-->
+        <template
+          slot="dateCell"
+          slot-scope="{date, data}">
+          <p :class="data.isSelected ? 'is-selected' : ''">
+            {{ data.day.split('-').slice(1).join('-') }} {{ data.isSelected ? '✔️' : ''}}
+          </p>
+        </template>
+      </el-calendar>
+    </div>
   </div>
 </template>
 
@@ -14,7 +26,7 @@ export default {
   },
   mounted () {
   },
-  methods () {
+  methods: {
   }
 }
 </script>
@@ -22,5 +34,22 @@ export default {
 <style lang="scss" scoped>
 .main {
   margin-top: 61px;
+  .header-title {
+    margin-left: 154px;
+    padding: 15px 0;
+  }
+  .calendar {
+    display: flex;
+    justify-content: center;
+    margin-bottom: 20px;
+    .calendar-content {
+      width: 1000px;
+      padding: 20px;
+    }
+  }
+}
+
+.is-selected {
+  color: #1989FA;
 }
 </style>
